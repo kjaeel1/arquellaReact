079f5f604b818fef3981f93f162bc3eb3601ab4b
SARFARAZ KHAN <sarfaraz.khan@embarkingonvoyage.com>
2023-05-08 15:44:41 +0530

    redux-RTK implemented done

--- src/modules/pages/DashBoardPage.jsx
+++ src/modules/pages/DashBoardPage.jsx
@@ -6,6 +6,10 @@ import Typography from "@mui/material/Typography";
 
 import axios from "axios";
 
+//redux imports
+import { useSelector, useDispatch } from 'react-redux'
+import { decrement, increment } from '../../redux/features/counter'
+
 function DashBoardPage() {
   const [open, setOpen] = React.useState(false);
 
@@ -18,41 +22,62 @@ function DashBoardPage() {
 
 
 
-    useEffect(()=> {
+  useEffect(() => {
 
-      axios.defaults.headers.common["Authorization"] = authToken;
+    axios.defaults.headers.common["Authorization"] = authToken;
 
-      axios
-        .post(apiUrl)
-    
-        .then((response) => {
-          console.log("565", JSON.stringify(response.data, null,2) );
-    
-          setResData([response.data]);
-        })
-    
-        .catch((error) => {
-          console.log(error);
-        });
-    },[])
+    axios
+      .post(apiUrl)
+
+      .then((response) => {
+        console.log("565", JSON.stringify(response.data, null, 2));
+
+        setResData([response.data]);
+      })
+
+      .catch((error) => {
+        console.log(error);
+      });
+  }, [])
+
+  const count = useSelector((state) => state.counter.value)
+  const dispatch = useDispatch()
+
 
   return (
     <div>
-    
-<p>
-  
+
+      <p>
 
 
 
-{resData.map(info => <div>{info.working}</div>)}
-  
- 
 
-  
-   
-</p>
- 
+        {resData.map(info => <div>{info.working}</div>)}
+
+
+
+
+
+      </p>
+
       <p>ddd</p>
+      <div>
+        <div>
+          <button
+            aria-label="Increment value"
+            onClick={() => dispatch(increment())}
+          >
+            Increment
+          </button>
+          <span>{count}</span>
+          <button
+            aria-label="Decrement value"
+            onClick={() => dispatch(decrement())}
+          >
+            Decrement
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
